0782503438ce89f610df52252837038928b9b95e
Andrei Ivanuta <124058262+andreiivanuta@users.noreply.github.com>
2026-08-21 12:08:29 +0200

    feat: add ingress-nginx controller via Helm

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -9,3 +9,12 @@ provider "azurerm" {
   #   local dev : az login (Azure CLI) + ARM_SUBSCRIPTION_ID
   #   CI        : GitHub OIDC via ARM_USE_OIDC + ARM_CLIENT_ID / ARM_TENANT_ID / ARM_SUBSCRIPTION_ID
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.this.kube_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate)
+  }
+}
